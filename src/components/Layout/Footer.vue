2019b11960ca9f79d62a95df57b03cfd007586ea
LadyVamp <ladyvamp666@gmail.com>
2021-07-21 15:20:56 +0300

    footer

--- src/components/Layout/Footer.vue
+++ src/components/Layout/Footer.vue
@@ -1,7 +1,10 @@
 <template>
     <v-footer padless>
         <v-col class="text-center" cols="12">
-            {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+            © {{ new Date().getFullYear() }}
+            <a href="https://ladyvamp.github.io/ng-portfolio/" target="_blank"
+                >Nadezhda Dyakova aka LadyVamp</a
+            >
         </v-col>
     </v-footer>
 </template>
