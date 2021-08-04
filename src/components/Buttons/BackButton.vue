f191e731cbb83b5526b4d40febbca073f9de8418
LadyVamp <ladyvamp666@gmail.com>
2021-08-04 16:48:09 +0300

    universal back button

--- src/components/Buttons/BackButton.vue
+++ src/components/Buttons/BackButton.vue
@@ -1,9 +1,9 @@
 <template>
-    <router-link to="/hikes">
+    <div @click="$router.go(-1)">
         <v-btn class="mt-2" color="accent" dark>
             <v-icon dark left> mdi-arrow-left </v-icon>Назад
         </v-btn>
-    </router-link>
+    </div>
 </template>
 
 <script>
