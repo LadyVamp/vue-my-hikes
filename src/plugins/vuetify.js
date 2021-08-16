33247b5c1f2c32ad2ff92b338981994214f7ed1e
LadyVamp <ladyvamp666@gmail.com>
2021-08-16 15:33:26 +0300

    russification

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,21 +3,22 @@ import Vuetify from 'vuetify/lib/framework';
 
 Vue.use(Vuetify);
 
-export default new Vuetify({
-    theme: {
-        themes: {
-          light: {
-            // primary: '#3f51b5',
-            // secondary: '#b0bec5',
-            // accent: '#8c9eff',
-            // error: '#b71c1c',
+import ru from 'vuetify/lib/locale/ru'
 
-            // https://colorscheme.ru/#2P31ToblOw0w0
-            primary: '#228B22',
-            secondary: '#AE662B',
-            accent: '#8B225E',
-            error: '#b71c1c',
-          },
-        },
+export default new Vuetify({
+  theme: {
+    themes: {
+      // https://colorscheme.ru/#2P31ToblOw0w0
+      light: {
+        primary: '#228B22',
+        secondary: '#AE662B',
+        accent: '#8B225E',
+        error: '#b71c1c',
       },
+    },
+  },
+  lang: {
+    locales: { ru },
+    current: 'ru',
+  },
 });
