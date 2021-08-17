b82ede927b5e84c97186e4aeb635e176570a4d6e
LadyVamp <ladyvamp666@gmail.com>
2021-08-17 11:45:24 +0300

    get version from package.json

--- src/components/Layout/TheFooter.vue
+++ src/components/Layout/TheFooter.vue
@@ -1,10 +1,23 @@
 <template>
     <v-footer padless>
-        <v-col class="text-center" cols="12">
+        <v-col class="text-center py-1" cols="12">
             © {{ new Date().getFullYear() }}
             <a href="https://ladyvamp.github.io/ng-portfolio/" target="_blank"
                 >Nadezhda Dyakova aka LadyVamp</a
             >
         </v-col>
+        <v-col class="text-right pt-0 pb-1 caption" cols="12">
+            {{ version }}
+        </v-col>
     </v-footer>
 </template>
+<script>
+import { version } from "../../../package.json";
+export default {
+    data() {
+        return {
+            version: "v" + version,
+        };
+    },
+};
+</script>
