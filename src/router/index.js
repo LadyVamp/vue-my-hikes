c97c976fb8fede6a6462f2e37af11f10c5af85e5
LadyVamp <ladyvamp666@gmail.com>
2021-08-25 11:53:34 +0300

    simplify router

--- src/router/index.js
+++ src/router/index.js
@@ -6,21 +6,19 @@ import VueYandexMetrika from 'vue-yandex-metrika';
 Vue.use(VueRouter);
 
 const routes = [
-  { path: '', redirect: '/hikes' },
   {
-    path: '/hikes',
-    name: 'Hikes',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('../views/Hike'),
+    path: "/",
+    component: () => import("@/views/Home"),
   },
   {
     path: '/hike/:id',
     component: HikeDetails
   },
-  { path: "*", redirect: '/hikes' },
-];
+  {
+    path: '*',
+    redirect: '/'
+  },
+]
 
 const router = new VueRouter({
   base: process.env.BASE_URL,
