613059b4f8cc9fc97c138aafb380ad713cbe430e
LadyVamp <ladyvamp666@gmail.com>
2021-09-08 12:59:58 +0300

    add pictures of gas cartridges

--- src/components/Hike/GasFlowCalculator.vue
+++ src/components/Hike/GasFlowCalculator.vue
@@ -15,22 +15,25 @@
         <v-row>
             <v-col cols="6">
                 <v-text-field
-                    label="Число человек"
-                    v-model="countPeople"
+                    label="Количество туристов"
+                    v-model="countTourist"
                     type="number"
                     min="1"
+                    max="30"
                 ></v-text-field>
                 <v-text-field
-                    label="Число готовок в день"
+                    label="Количество готовок в день"
                     v-model="countCooksPerDay"
                     type="number"
                     min="1"
+                    max="3"
                 ></v-text-field>
                 <v-text-field
-                    label="Число дней"
+                    label="Количество дней"
                     v-model="countDays"
                     type="number"
                     min="1"
+                    max="30"
                 ></v-text-field>
                 <v-text-field
                     label="Время закипания 1 литра воды"
@@ -57,8 +60,37 @@
         </v-row>
         <v-row v-if="gasWeight !== 0" class="d-flex flex-column">
             <v-col>Результат, г: {{ gasWeight }} </v-col>
-            <v-col>
-                Баллонов, шт (1 баллон 450 г): {{ Math.ceil(gasWeight / 450) }}
+            <v-col v-if="gasWeight <= 100">
+                Баллонов, шт (1 баллон <b>100</b> г):
+                {{ Math.ceil(gasWeight / 100) }}
+                <figure>
+                    <img src="../../assets/gas/gas100.jpg" alt="" />
+                </figure>
+            </v-col>
+            <v-col v-else-if="gasWeight > 100 && gasWeight <= 230">
+                Баллонов, шт (1 баллон <b>230</b> г):
+                {{ Math.ceil(gasWeight / 230) }}
+                <figure>
+                    <img src="../../assets/gas/gas230.jpg" alt="" />
+                </figure>
+            </v-col>
+            <v-col v-else>
+                Баллонов, шт (1 баллон <b>450</b> г):
+                {{ gas450Cartridge }}      
+                    
+                <figure v-if="gas450Cartridge < 20">
+                    <img
+                        v-for="n in gas450Cartridge"
+                        :key="n"
+                        src="../../assets/gas/gas450.jpg"
+                    />
+                </figure>
+                <figure v-else class="d-flex">
+                    <img src="../../assets/gas/gas450.jpg" alt="" />
+                    <p class="py-4">
+                        × {{ gas450Cartridge }}
+                    </p>                        
+                </figure>
             </v-col>
         </v-row>
     </v-container>
@@ -66,26 +98,26 @@
 
 <script>
 export default {
-    /* 
-        Число человек	2
-        Число готовок в день	2
-        Число дней	7
-        Время закипания	4 минуты 43 секунды
-        Расход газа	140 (г/час)
-    */
     data() {
         return {
-            countPeople: 2,
+            countTourist: 2,
             countCooksPerDay: 2,
-            countDays: 7,
+            countDays: 10,
             boilingTime: "04:43",
             gasConsumption: 140,
             gasWeight: 0,
+            gas450Cartridge: 0,
         };
     },
     methods: {
+        /**
+         * Вес газа = Объем кана * Расход газа на 1 литр воды * Кол-во туристов * Кол-во готовок в день * Кол-во дней
+         * где
+         *      Объем кана = пол-литра на человека
+         *      Расход газа на 1 литр воды = Время закипания 1 литра воды (сек) * Расход газа (г/час) / 3600
+         */
         calculateGasWeight() {
-            const potVolume = 0.5 * this.countPeople;
+            const potVolume = 0.5 * this.countTourist;
 
             const boilingTimeInSeconds =
                 +this.boilingTime.split(":")[0] * 60 +
@@ -97,11 +129,13 @@ export default {
             const result =
                 potVolume *
                 gasConsumptionPerOneLiter *
-                this.countPeople *
+                this.countTourist *
                 this.countCooksPerDay *
                 this.countDays;
 
             this.gasWeight = Math.round(result);
+
+            this.gas450Cartridge = Math.ceil(this.gasWeight / 450);
 
             console.group("calculateGasWeight");
             console.log("potVolume", potVolume);
@@ -109,6 +143,7 @@ export default {
             console.log("gasConsumptionPerOneLiter", gasConsumptionPerOneLiter);
             console.log("result", result);
             console.log("Math.ceil(result)", Math.ceil(result));
+            console.log("gas450Cartridge", this.gas450Cartridge);
             console.groupEnd();
 
             return this.gasWeight;
@@ -117,8 +152,14 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 .v-input {
     width: 200px;
+}
+img {
+    height: 100px;
+}
+figure p {
+    font-size: 40px;
 }
 </style>
