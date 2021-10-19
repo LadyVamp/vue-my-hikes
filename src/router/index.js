d1985cf01a9c0ba8aae29f4f0074c4d636a2f84e
LadyVamp <ladyvamp666@gmail.com>
2021-10-19 09:47:13 +0300

    fix metrika

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import HikeDetails from '@/components/Hike/HikeDetails';
-import VueYandexMetrika from 'vue-yandex-metrika';
 
 Vue.use(VueRouter);
 
@@ -28,11 +27,5 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes,
 });
-
-Vue.use(VueYandexMetrika, {
-  id: 84106609,
-  router: router,
-  env: process.env.NODE_ENV
-})
 
 export default router;
