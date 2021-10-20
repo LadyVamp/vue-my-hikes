7e87b9da8f1473022c130b95051387e62bfc717b
LadyVamp <ladyvamp666@gmail.com>
2021-10-20 10:31:08 +0300

    fix metrika

--- src/main.js
+++ src/main.js
@@ -6,14 +6,24 @@ import axios from 'axios';
 import VueAxios from 'vue-axios';
 import VueMeta from 'vue-meta';
 import { store } from './store';
+import VueYandexMetrika from 'vue-yandex-metrika';
 
 Vue.config.productionTip = false;
 Vue.use(VueAxios, axios);
 Vue.use(VueMeta);
+Vue.use(VueYandexMetrika, {
+	id: 84106609,
+	router: router,
+	env: process.env.NODE_ENV,
+	clickmap: true,
+	trackLinks: true,
+	accurateTrackBounce: true,
+	webvisor: true
+})
 
 new Vue({
-  store,
-  router,
-  vuetify,
-  render: (h) => h(App),
+	store,
+	router,
+	vuetify,
+	render: (h) => h(App),
 }).$mount('#app');
