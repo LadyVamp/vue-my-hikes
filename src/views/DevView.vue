ffda8bc4b0d287c81318a6b8a75269434d749a32
LadyVamp <ladyvamp666@gmail.com>
2026-06-29 15:37:40 +0300

    upd dev

--- src/views/DevView.vue
+++ src/views/DevView.vue
@@ -1,22 +1,15 @@
 <template>
   <div>
-    <h2 class="primary--text">Выгрузка треков из Strava</h2>
+    <h2 class="primary--text">Выгрузка треков из Organic Maps</h2>
     <ol>
-      <li>На телефоне: Включить VPN, открыть Strava, найти трек</li>
-      <li>Рассказать о тренировке → Еще → VK → отправить самому себе</li>
-      <li>Отправится текст: Оцени мою тренировку в Strava: https://strava.app.link/some_hash</li>
-      <li>Выключить VPN</li>
-      <li>Открыть ссылку на десктопе</li>
-      <li>
-        Скопировать часть ссылки вида https://www.strava.com/activities/some_id
-      </li>
+      <li>Списки → Мои метки</li>
+      <li>Три точки → Экспорт GPX → Отправить самому себе в VK</li>
+      <li>Скачать из VK</li>
       <li>
         Открыть
         <a href="https://nakarte.me/#m=9/55.73020/37.97424&l=O" target="_blank">nakarte.me</a>
       </li>
-      <li>
-        Вставить ссылку в инпут Track URL
-      </li>
+      <li>Открыть файл</li>
     </ol>
   </div>
 </template>
